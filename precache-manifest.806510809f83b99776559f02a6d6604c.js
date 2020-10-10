0478f6430aec5bf53f8b41aa76b0e6e636867e47
mohithg <mohithgm@gmail.com>
2020-10-10 09:22:40 +0000

    Updates

---
+++ precache-manifest.806510809f83b99776559f02a6d6604c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc6e78a387e6140cd0c9373f2032b704",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7db3c43465e78a20450e",
+    "url": "/static/css/2.a1c9976d.chunk.css"
+  },
+  {
+    "revision": "3a842c58aef870aff778",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7db3c43465e78a20450e",
+    "url": "/static/js/2.46203f2a.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.46203f2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a842c58aef870aff778",
+    "url": "/static/js/main.034cc40e.chunk.js"
+  },
+  {
+    "revision": "dc295eff9549f3501b46",
+    "url": "/static/js/runtime-main.7e5e325a.js"
+  },
+  {
+    "revision": "9d816344d6b1e8e7d5823ac5a619bc19",
+    "url": "/static/media/FresaLabs.9d816344.png"
+  }
+]);
