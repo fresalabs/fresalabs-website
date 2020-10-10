ff5bf58c523a0d495b99c80b2437c2615fa2231c
mohithg <mohithgm@gmail.com>
2020-10-10 09:16:32 +0000

    Updates

---
+++ precache-manifest.6754d6770d1ec7fe80ef5f6444929ab7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad8cc80583380fca9a07dc69da8014de",
+    "url": "/fresalabs/index.html"
+  },
+  {
+    "revision": "d52b79b85c77f3d5b24d",
+    "url": "/fresalabs/static/css/2.a1c9976d.chunk.css"
+  },
+  {
+    "revision": "fd074f0bcf1959048a1b",
+    "url": "/fresalabs/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d52b79b85c77f3d5b24d",
+    "url": "/fresalabs/static/js/2.7c3bcd83.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/fresalabs/static/js/2.7c3bcd83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd074f0bcf1959048a1b",
+    "url": "/fresalabs/static/js/main.b852fe3b.chunk.js"
+  },
+  {
+    "revision": "a7580fc0253ca8a4634a",
+    "url": "/fresalabs/static/js/runtime-main.cfe1299f.js"
+  },
+  {
+    "revision": "9d816344d6b1e8e7d5823ac5a619bc19",
+    "url": "/fresalabs/static/media/FresaLabs.9d816344.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.806510809f83b99776559f02a6d6604c.js"
+  "/fresalabs/precache-manifest.6754d6770d1ec7fe80ef5f6444929ab7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fresalabs/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
