7c0906d16db9861c1c7609f10070205c50277fdc
snkreddy <snkreddy1@gmail.com>
2020-10-11 11:48:22 +0530

    added products

--- src/home/Products.tsx
+++ src/home/Products.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { map } from 'ramda';
+import { map, filter, isNil } from 'ramda';
 import styled from 'styled-components';
 import { Card } from 'antd';
 import StarIcon from '@material-ui/icons/Star';
@@ -7,6 +7,8 @@ import GitHubIcon from '@material-ui/icons/GitHub';
 import { Typography } from '@material-ui/core';
 import ProductsHuntIcon from './producthunt.png';
 import NpmIcon from './npm.png';
+import ClipBoardX from '../icons/ClipboardXIcon';
+import CSSSpiderIcon from '../icons/CSSSpider';
 
 const { Meta } = Card;
 
@@ -21,15 +23,22 @@ const ProductsContainer = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: row;
+  .ant-card {
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    margin: 8px;
+  }
 `;
 
 interface Product {
   name: string;
   description: string;
   siteLink: string;
-  githubLink: string;
-  productsHuntLink: string;
+  githubLink?: string;
+  productsHuntLink?: string;
   npmLink?: string;
+  icon: React.ReactNode;
 }
 
 const products: Product[] = [
@@ -40,11 +49,28 @@ const products: Product[] = [
     npmLink: 'https://www.npmjs.com/package/@fresalabs/sleekform',
     productsHuntLink:
       'https://www.producthunt.com/posts/sleekform?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-sleekform',
-    description: 'Typeform components built with React',
+    description: 'Typeform components built with React, Sleek-Form offers complete control over all the components of Typeform',
+    icon: <StarIcon />
+
+  },
+  {
+    name: 'ClipboardX',
+    siteLink: 'https://clipboardx.fresalabs.com/home',
+    githubLink: 'https://github.com/fresalabs',
+    description: 'A light Apple app that lives on your menu bar maintains the history of you copied text',
+    icon: <ClipBoardX />
+  },
+  {
+    name: 'CSS Spider',
+    siteLink: 'https://cssspider.fresalabs.com/home',
+    githubLink: 'https://github.com/fresalabs',
+    description: 'The fastest and easiest way to check, copy and edit CSS.',
+    icon: <CSSSpiderIcon />
   },
 ];
 
 class Products extends React.Component {
+
   render() {
     return (
       <div>
@@ -54,32 +80,33 @@ class Products extends React.Component {
             (product: Product) => (
               <Card
                 style={{ width: 300 }}
-                actions={[
-                  <a key="github" href={product.githubLink}>
-                    <GitHubIcon />
-                  </a>,
-                  product.productsHuntLink && (
-                    <a key="npm" href={product.productsHuntLink}>
-                      <img
-                        src={ProductsHuntIcon}
-                        alt="Sleekform - Fully customizable Typeform components | Product Hunt"
-                        style={{ width: 25, height: 25 }}
-                      />
-                    </a>
-                  ),
-                  product.npmLink && (
-                    <a key="npm" href={product.npmLink}>
-                      <img
-                        style={{ width: 20, height: 20 }}
-                        src={NpmIcon}
-                        alt="Sleekform - Fully customizable Typeform components | NPM"
-                      />
-                    </a>
-                  ),
-                ]}
+                actions={
+                  filter(action => !isNil(action),[
+                    product.githubLink && (<a key="github" href={product.githubLink}>
+                      <GitHubIcon />
+                    </a>),
+                    product.productsHuntLink && (
+                      <a key="npm" href={product.productsHuntLink}>
+                        <img
+                          src={ProductsHuntIcon}
+                          alt="Sleekform - Fully customizable Typeform components | Product Hunt"
+                          style={{ width: 25, height: 25 }}
+                        />
+                      </a>
+                    ),
+                    product.npmLink && (
+                      <a key="npm" href={product.npmLink}>
+                        <img
+                          style={{ width: 20, height: 20 }}
+                          src={NpmIcon}
+                          alt="Sleekform - Fully customizable Typeform components | NPM"
+                        />
+                      </a>
+                    ),
+                ])}
               >
                 <a href={product.siteLink}>
-                  <Meta avatar={<StarIcon />} title={product.name} description={product.description} />
+                  <Meta avatar={product.icon} title={product.name} description={product.description} />
                 </a>
               </Card>
             ),
---
+++ src/icons/CSSSpider.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const CSSSpiderIcon = () => {
+  return (
+    <div>
+      <svg
+        version="1.0"
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        viewBox="0 0 289.000000 175.000000"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        <metadata>Created by potrace 1.16, written by Peter Selinger 2001-2019</metadata>
+        <g transform="translate(0.000000,175.000000) scale(0.100000,-0.100000)" fill="#800080" stroke="none">
+          <path
+            d="M1265 1415 c-293 -40 -496 -148 -761 -407 l-156 -153 155 -151 c155
+-151 258 -230 384 -293 288 -144 636 -166 948 -59 194 66 334 161 548 370
+l137 135 -140 137 c-164 159 -264 237 -387 298 -233 117 -473 158 -728 123z
+m309 -197 c98 -38 178 -116 222 -218 15 -35 19 -68 19 -150 0 -100 -2 -108
+-33 -165 -80 -147 -190 -215 -347 -215 -132 0 -237 51 -310 150 -58 79 -79
+141 -79 240 0 76 3 92 33 154 44 90 96 145 174 185 104 53 217 60 321 19z"
+          />
+          <path
+            d="M1334 1011 c-95 -68 -114 -168 -51 -262 77 -114 256 -101 316 23 63
+131 -25 268 -172 268 -44 0 -62 -6 -93 -29z"
+          />
+        </g>
+      </svg>
+    </div>
+  );
+};
+
+export default CSSSpiderIcon;
